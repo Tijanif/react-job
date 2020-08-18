1992f5dad3d01de31590d28ca92cdd840d643802
Tijani Amadu Fulani <tijanifulani@tijanis-air.home>
2020-08-18 17:11:15 +0100

    Updates

---
+++ precache-manifest.cdfecac2543111e193bf3300318504f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2106c0ea2ed91a128e2e94fdd4ac13",
+    "url": "/react-job/index.html"
+  },
+  {
+    "revision": "f950637b0cb5ac953474",
+    "url": "/react-job/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "a0e9120ae5533ec13a1c",
+    "url": "/react-job/static/js/2.065765ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-job/static/js/2.065765ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f950637b0cb5ac953474",
+    "url": "/react-job/static/js/main.7a2bb98a.chunk.js"
+  },
+  {
+    "revision": "fa41b431f671b3a0f96d",
+    "url": "/react-job/static/js/runtime-main.4f77e493.js"
+  }
+]);
